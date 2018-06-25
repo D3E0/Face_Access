96c2d149dd35c1890668bafb85c3684f154f0aab
leonyan18 <33154558+leonyan18@users.noreply.github.com>
2018-06-25 18:51:18 +0800

    all

--- Face_Access/src/main/java/controller/LoginController.java
+++ Face_Access/src/main/java/controller/LoginController.java
@@ -1,13 +1,21 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import service.FaceMangeService;
 import util.Face;
 
 @Controller
 public class LoginController {
+    private FaceMangeService faceService;
+    @Autowired
+    public void setFaceService(FaceMangeService faceService) {
+        this.faceService = faceService;
+    }
+
     @RequestMapping("/login")
     public String login(){
         return "login";
@@ -16,6 +24,6 @@ public class LoginController {
     @ResponseBody
     public String checkpic(@RequestParam(value = "img",defaultValue = "0") String imgStr){
         imgStr=imgStr.replaceFirst("data:image/jpeg;base64,", "");
-        return Face.search(imgStr);
+        return faceService.searchface(imgStr);
     }
 }
